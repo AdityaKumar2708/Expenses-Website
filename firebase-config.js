1e6f894413539ae2b0ba25ff66b6f026a494f574
Aditya Kumar <adityakumar2708@gmail.com>
2026-04-08 01:27:12 +0530

    Fix Firebase auth wiring

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBX5S4Y945fx7Z6hrZQbKA-7N1LhBfsXr0",
+  authDomain: "expenses-system-ac6fb.firebaseapp.com",
+  projectId: "expenses-system-ac6fb",
+  storageBucket: "expenses-system-ac6fb.firebasestorage.app",
+  messagingSenderId: "645826905383",
+  appId: "1:645826905383:web:f0f38cc25807b4ae29d502",
+  measurementId: "G-KZC63GELXD",
+};
